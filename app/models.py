5eda62fca2c6cd701a7f08db26471a7de69fbaab
karlokraml2 <karlokraml2@gmail.com>
2024-01-24 09:29:44 +0100

    jeej radi

--- app/models.py
+++ app/models.py
@@ -6,21 +6,39 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     date_of_birth = models.DateTimeField
-    gender = models.CharField(max_lenght=32)
-    avatar_url = models.CharField(max_lenght=128)
+    gender = models.CharField(max_length=32)
+    avatar_url = models.CharField(max_length=128)
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        related_name='afterios_group',  
+        help_text=(
+            'The groups this user belongs to. A user will get all permissions '
+            'granted to each of their groups.'
+        ),
+    )
+
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        related_name='afterios_permissions', 
+        help_text='Specific permissions for this user.',
+    )
 
 
 class PartyPosters(models.Model):
-    party_url = models.CharField(max_lenght=128)
+    party_url = models.CharField(max_length=128)
 
 
 class Party(models.Model):
-    title = models.CharField(max_lenght=128)
+    title = models.CharField(max_length=128)
     total_allowed_guest = models.SmallIntegerField
-    description = models.CharField(max_lenght=1024)
+    description = models.CharField(max_length=1024)
     created_at = models.DateTimeField(auto_now_add=True)
     closed_at = models.DateTimeField()
-    location = models.CharField(max_lenght=128)
+    location = models.CharField(max_length=128)
     party_poster_fk = models.ForeignKey(PartyPosters, on_delete=models.CASCADE)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -28,7 +46,7 @@ class Party(models.Model):
 class Recension(models.Model):
     party_id = models.ForeignKey(Party, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.CharField(max_lenght=512)
+    text = models.CharField(max_length=512)
     rating = models.SmallIntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
