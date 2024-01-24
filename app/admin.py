267f8db3df19680b21a17bfd272b6739b5db211e
Fran Josipovic <fran.josipovic@atos.net>
2024-01-24 10:15:41 +0100

    add party request model

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import Party, PartyGuest, PartyPosters, PartyRequest, Recension, User
+
+admin.site.register(User)
+admin.site.register(Party)
+admin.site.register(PartyPosters)
+admin.site.register(Recension)
+admin.site.register(PartyGuest)
+admin.site.register(PartyRequest)
